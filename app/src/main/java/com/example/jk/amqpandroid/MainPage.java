741a9306c36e8ba2125b83f3ab4cadf7f5c032ec
jaakko <jaakko>
2016-03-05 14:05:31 +0100

    restructure

--- app/src/main/java/com/example/jk/amqpandroid/MainPage.java
+++ app/src/main/java/com/example/jk/amqpandroid/MainPage.java
@@ -3,11 +3,11 @@ package com.example.jk.amqpandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SendOrReceive extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_or_receive);
+        setContentView(R.layout.activity_main_page);
     }
 }
